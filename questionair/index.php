0cf9fb6b2fe3eaa31889ede39b83dadacbfab6d2
Sander Latour <latour@uva.nl>
2013-01-28 14:34:27 +0100

    Finished introduction in LA, started on personalized learning

--- questionair/index.php
+++ questionair/index.php
@@ -17,41 +17,59 @@ function generate($bits, $numflip, $dim){
 $features = array();
 $features[] = array(
   "This is an individual assignment",
-  "On this assignment you work in groups"
+  "On this assignment you work in groups",
+  "On this type of assignment you are asked to work alone",
+  "On this type of assignment you are asked to work in a group and cooperate on the final product"
 );
 $features[] = array(
   "Each individual sub-assignment will be graded",
-  "The grade for the assignment is based on the final product"
+  "The grade for the assignment is based on the final product",
+  "This is an assignment that is split up into several smaller assignments or exercises. The smaller assignments are not necessarily handed in separately. For instance, an assignment consisting of multiple exercises for which each exercise is graded",
+  "This is a large assignment for which the grade is based on the complete assignment"
 );
 $features[] = array(
   "Your work will be graded by your fellow students",
-  "Your work is graded by the TA"
+  "Your work is graded by the TA",
+  "This assignment is peer-reviewed which means that your grade is given to you by a fellow student. Naturally this happens under the moderation of the teaching assistant",
+  "The grade for this assignment is given by the teaching assistant"
 );
 $features[] = array(
   "",
-  "The assignments will be graded relative to other students"
+  "The assignments will be graded relative to other students",
+  "",
+  "Your grade is based on the performance of other students. For instance, when all students have presented the grades are based on how well other students presented. Another possibility is that the practical is in the form of a competition for which the highest score results in the highest grade"
 );
 $features[] = array(
   "This assignment is for your own personal benefit and thus the grade for ".
     "this assignment will not be considered for the final grade of the course",
+  "",
+  "The assignment is not considered in the final computation of your grade for the course. The assignment is still mandatory and you might receive a grade, but the grade is only meant as feedback",
   ""
 );
 $features[] = array(
   "",
   "During the assignment, the TA can give special badges that you can earn ".
-    "for being most creative or most innovative or for best performance"
-  );
+    "for being most creative or most innovative or for best performance",
+  "",
+  "By achieving certain goals or performing certain tasks you can receive badges. These badges represent things like being the first to hand in the assignment, or getting the best result, being most innovative or creative, being the last to hand it in, doing extra work"
+);
 $features[] = array(
   "",
-  "Students that perform well can attend a masterclass on the subject"
+  "Students that perform well can attend a masterclass on the subject",
+  "",
+  "By performing well on the assignment, you will get access to some extra content. In this case, the extra content is in the form of a master class given by a famous professor which you can attend"
 );
 $features[] = array(
   "The deadline is only meant as a guideline and will not be enforced",
-  "There is a strict deadline for this assignment"
+  "There is a strict deadline for this assignment",
+  "On this assignment there is not a strict deadline. Naturally, there is a date to hand in your work, but this date is meant as a guideline and it will not be enforced",
+  "On this assignment there is a strict deadline. Failing to hand in your work before this deadline also means you fail the assignment and your grade will be modified accordingly"
 );
 $features[] = array(
   "You will hand in multiple sub-assignments",
-  "All work will be handed in at the end of the assignment"
+  "All work will be handed in at the end of the assignment",
+  "The assignment has multiple moments in which the work so far has to be handed in. This does not mean that each separate assignment is graded",
+  "The assignment is handed in as one complete work. However, the assignment may consist of multiple parts that are graded separately"
 );
 
 // Constants
@@ -68,45 +86,59 @@ if(isset($_REQUEST['motivation']) && $_REQUEST['motivation'] != ""){
   for($i = 0; $i<$dim; $i++){
     $configstr .= ($configstr==""?"":",").(($config>>($dim-1-$i))&0x1);
   }
-  $_SESSION['answers'][] = sprintf("%s,%s,%s",$_SESSION['meta'],$configstr,$_REQUEST['motivation']);
+  array_shift($_SESSION['configs']);
+  $to = "gameddata@gmail.com";
+  $from = "latour@uva.nl";
+  $subject = session_id();
+  $body = sprintf("%s,%s,%s",$_SESSION['meta'],$configstr,$_REQUEST['motivation']);
+  mail($to, $subject, $body, "From: $from");
 }
 
-// If the list of feature configurations is empty, generate it
-if(
-  (
-    !isset($_SESSION['answers']) || 
-    sizeof($_SESSION['answers']) == 0
-  ) &&
-  (
-    !isset($_SESSION['configs']) || 
-    sizeof($_SESSION['configs']) == 0
-  )
-){
-  $_SESSION['configs'] = array($n);
-  
+
+$meta_error = "";
+if(isset($_REQUEST['program']) && $_REQUEST['program'] != ""){
+  if(
+    $_REQUEST['program'] == "none" ||
+    $_REQUEST['level'] == "none" ||
+    $_REQUEST['gender'] == "none" ||
+    $_REQUEST['generalmot'] == "none"
+  ){
+    $meta_error = "<span style='color: red'>Please fill in all questions.</span><br />";
+  }else{
+    $_SESSION['meta'] = $_REQUEST['program'].",".$_REQUEST['level'].",".$_REQUEST['gender'].",".$_REQUEST['generalmot'];
+  }
+}
+
+if(isset($_REQUEST['f']) && $_REQUEST['f']==0){
+  $_SESSION['configs'] = array();
+
   // Generate $n configurations
   $bits = 0x0;
   for($i = 0; $i < $n; $i++){
     $bits = generate($bits, $numflip, $dim);
     $_SESSION['configs'][] = $bits;
   }
-}
-
-$meta_error = "";
-if(isset($_REQUEST['program']) && $_REQUEST['program'] != ""){
-  if(
-    $_REQUEST['program'] == "none" ||
-    $_REQUEST['gender'] == "none" ||
-    $_REQUEST['generalmot'] == "none"
-  ){
-    $meta_error = "<span style='color: red'>Please fill in all questions.</span><br />";
-  }else{
-    $_SESSION['meta'] = $_REQUEST['program'].",".$_REQUEST['gender'].",".$_REQUEST['generalmot'];
-  }
+  unset($_REQUEST['f']);
+  header("Location: ./");
 }
 
 // If the meta questions have not been stored yet
 if( !isset($_SESSION['meta']) || $_SESSION['meta'] == ""){
+
+  // If the list of feature configurations is empty, generate it
+  if(
+    !isset($_SESSION['configs']) || 
+    sizeof($_SESSION['configs']) == 0
+  ){
+    $_SESSION['configs'] = array();
+    
+    // Generate $n configurations
+    $bits = 0x0;
+    for($i = 0; $i < $n; $i++){
+      $bits = generate($bits, $numflip, $dim);
+      $_SESSION['configs'][] = $bits;
+    }
+  }
 ?>
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
 <html>
@@ -118,15 +150,20 @@ if( !isset($_SESSION['meta']) || $_SESSION['meta'] == ""){
 Educational program:
 <select name='program'>
   <option value='none'>Select what fits best</option>
-  <option value='0'>First year BSc Kunstmatige Intelligentie</option>
-  <option value='1'>Second year BSc Kunstmatige Intelligentie</option>
-  <option value='2'>Third year BSc Kunstmatige Intelligentie</option>
-  <option value='3'>First year BSc Informatica</option>
-  <option value='4'>Second year BSc Informatica</option>
-  <option value='5'>Third year BSc Informatica</option>
-  <option value='6'>BSc Beta-gamma</option>
-  <option value='7'>MSc Artificial Intelligence</option>
-  <option value='8'>Other</option>
+  <option value='0'>Artificial Intelligence</option>
+  <option value='1'>Computer Science</option>
+  <option value='2'>Information Sciences (Informatiekunde)</option>
+  <option value='3'>Beta-gamma</option>
+  <option value='4'>Other</option>
+</select><br />
+Level:
+<select name='level'>
+  <option value='none'>Select what fits best</option>
+  <option value='0'>First year Bachelor</option>
+  <option value='1'>Second year Bachelor</option>
+  <option value='2'>Third year Bachelor</option>
+  <option value='3'>First year Master</option>
+  <option value='4'>Second year Master</option>
 </select><br />
 Gender:
 <select name='gender'>
@@ -150,27 +187,50 @@ Motivation in general:
 </html>
 <?php
 // If there are still configurations to ask
-} else if(sizeof($_SESSION['configs'])>1){
+} else if(sizeof($_SESSION['configs'])>0){
   // Pick the first unused configuration
-  $config = array_shift($_SESSION['configs']);
+  $config = $_SESSION['configs'][0];
 
   //DEBUG: printf("%0".$dim."b - %d left<br />",$config,sizeof($_SESSION['configs']));
 
   $strings = array();
+  $explanations = array();
   // Verbalize the configuration
   for($i = 0; $i < $dim; $i++){
     $str = $features[$i][($config >> $i) & 0x1];
+    $exp = $features[$i][(($config >> $i) & 0x1)+2];
     if($str!=""){
       $strings[] = $str;
+      $explanations[] = $exp;
     }
   }
 
-  shuffle($strings);
+  $keys = array_rand($strings, sizeof($strings));
+  $tmp1 = array();
+  $tmp2 = array();
+  foreach($keys as $key){
+    $tmp1[] = $strings[$key];
+    $tmp2[] = $explanations[$key];
+  }
+  $strings = $tmp1;
+  $explanations = $tmp2;
 ?>
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
 <html>
 <head><title>Survey</title></head>
 <body>
+<style>
+.small {
+  font-size: small;
+}
+.bold {
+  font-weight: bold;
+}
+.title {
+  font-size: large;
+}
+</style>
+<span class='small bold'>
 <?php
 if(sizeof($_SESSION['configs'])==1){
   echo "This is the last question.";
@@ -178,13 +238,18 @@ if(sizeof($_SESSION['configs'])==1){
   echo "Just ".sizeof($_SESSION['configs'])." more questions to go.";
 }
 ?>
-<ul>
+</span>
+<div class='title'>
+The following attributes are mentioned in the assignment description, describe your initial motivation.
+</div>
+<ol>
 <?php
 for($i = 0; $i < sizeof($strings); $i++){
   echo "<li>".$strings[$i]."</li>";
 }
 ?>
-</ul>
+</ol>
+<div class='title'>Initial motivation:</div>
 <form method="POST">
 <input type='hidden' name='<?php echo session_name();?>' value='<?php echo session_id(); ?>' />
 <input type='hidden' name='config' value='<?php echo $config; ?>' />
@@ -196,19 +261,24 @@ for($i = 0; $i < sizeof($strings); $i++){
 <br />
 <input type='submit' value='Next' />
 </form>
+<br />
+<hr>
+<div class='title'>Explanations:</div>
+<ol class='small'>
+<?php
+for($i = 0; $i < sizeof($explanations); $i++){
+  echo "<li>".$explanations[$i]."</li>";
+}
+?>
+</ol>
 </body>
 </html>
 <?php 
 // If all configurations have been asked, close and thank.
 }else{
-
-$to = "gameddata@gmail.com";
-$from = "latour@uva.nl";
-$subject = "[gameddata-".time()."]";
-$body = implode("\n",$_SESSION['answers']);
-mail($to, $subject, $body, "From: $from");
-$_SESSION = array();
-session_destroy();
+  if(isset($_REQUEST['f']) && $_REQUEST['f']==1){
+    $_SESSION = array();
+    session_destroy();
 ?>
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
 <html>
@@ -218,5 +288,18 @@ session_destroy();
 </body>
 </html>
 <?php
+  }else{
+?>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+<html>
+<head><title>Survey</title></head>
+<body>
+<h2>This was very helpful!</h2>
+<a href='./?f=1'>I'm done, get me out of here!</a><br />
+<a href='./?f=0'>I could do some more, if that makes you happy!</a>
+</body>
+</html>
+<?php
+  }
 }
 ?>
